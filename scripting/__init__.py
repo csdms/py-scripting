ee1d7b75bf2fcbeea5896be8cc5f68292b5b8043
Mark Piper <mark.piper@colorado.edu>
2023-12-12 17:20:37 -0700

    Use importlib.metadata over deprecated pkg_resources

--- scripting/__init__.py
+++ scripting/__init__.py
@@ -1,7 +1,7 @@
-import pkg_resources
+from importlib.metadata import version
 from .contexts import cd
 from .prompting import error, prompt, status, success
 from .unix import cp, ln_s
 
-__version__ = pkg_resources.get_distribution("py-scripting").version
+__version__ = version("py-scripting")
 __all__ = ["prompt", "status", "success", "error", "cp", "cd", "ln_s"]
